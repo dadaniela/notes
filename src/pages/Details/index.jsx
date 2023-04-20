dea0ffbaa20874f148c8c9a6ccaceb3072c478eb
Daniela <trindadedv@gmail.com>
2023-04-20 15:02:30 +0200

    adding global styles and using ThemeProvider

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,10 @@
-import {Container} from "./styles"
-export function Details(){
+import { Container } from "./styles";
 
+export function Details(){
   return(
     <Container>
       <h1>Hello World!</h1>
       <span>Daniela</span>
     </Container>
   )
-}
+};
