75a7eaee93cf28538aaa46ffb6a689b6c773a616
Daniela <trindadedv@gmail.com>
2023-05-10 17:47:56 +0200

    styling the grid area brand in the home page

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -15,16 +15,24 @@ export const Container = styled.div`
 `;
 export const Brand = styled.div`
     grid-area: brand;
-    background: pink;
-    color: ${({theme}) => theme.COLORS.WHITE};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-bottom-width: 1px;
+    border-bottom-style: solid;
+    border-bottom-color: ${({theme}) => theme.COLORS.BACKGROUND_700};
+    background: ${({theme}) => theme.COLORS.BACKGROUND_900};
+    
+    > h1 {
+        font-size: 24px;
+        color: ${({theme}) => theme.COLORS.ORANGE};
+    }
 `;
 export const Menu = styled.ul`
     grid-area: menu;
-    background: gray;
 `;
 export const Search = styled.div`
     grid-area: search;
-    background: lightblue;
 
 
 `;
