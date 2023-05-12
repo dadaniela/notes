ef6938817e1cff875a01f98edc749f06b92309cf
Daniela <trindadedv@gmail.com>
2023-05-12 21:21:04 +0200

    sign in and sign up pages

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,21 +1,23 @@
-import { FiMail, FiLock } from "react-icons/fi";
+import { FiMail, FiLock, FiUser } from "react-icons/fi";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 import { Container, Form, HomeImg } from "./styles";
 
-export function SignIn(){
+export function SignUp(){
     return(
         <Container>
+            <HomeImg alt="home image: sticky notes"></HomeImg>
             <Form>
                 <h1>Notes</h1>
                 <p>Save and manage your notes in one place</p>
-                <h2>Log in</h2>
+                <h2>Create account</h2>
+                <Input placeholder="Name" type="text" icon={FiUser}/>
                 <Input placeholder="Email" type="text" icon={FiMail}/>
                 <Input placeholder="Password" type="password" icon={FiLock}/>
-                <Button label="Log in"/>
+                <Button label="Sign up"/>
                 <a href="#">Sign in</a>
             </Form>
-            <HomeImg alt="home image: sticky notes"></HomeImg>
+            
         </Container>
     );
 }
