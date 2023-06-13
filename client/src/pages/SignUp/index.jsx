52914b5a1caf7837f8c3645cf36fdc993e4497c8
Daniela <trindadedv@gmail.com>
2023-06-13 17:45:24 +0200

    handling create account input

--- client/src/pages/SignUp/index.jsx
+++ client/src/pages/SignUp/index.jsx
@@ -1,10 +1,17 @@
 import { FiMail, FiLock, FiUser } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 import { Container, Form, HomeImg } from "./styles";
 
 export function SignUp(){
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    function handleSignUp(){
+        console.log(name, email, password)
+    }
     return(
         <Container>
             <HomeImg alt="home image: sticky notes"></HomeImg>
@@ -12,10 +19,25 @@ export function SignUp(){
                 <h1>Notes</h1>
                 <p>Save and manage your notes in one place</p>
                 <h2>Create account</h2>
-                <Input placeholder="Name" type="text" icon={FiUser}/>
-                <Input placeholder="Email" type="text" icon={FiMail}/>
-                <Input placeholder="Password" type="password" icon={FiLock}/>
-                <Button label="Sign up"/>
+                <Input
+                    placeholder="Name"
+                    type="text"
+                    icon={FiUser}
+                    onChange={e => setName(e.target.value)}
+                />
+                <Input
+                    placeholder="Email"
+                    type="text"
+                    icon={FiMail}
+                    onChange={e => setEmail(e.target.value)}
+                />
+                <Input
+                    placeholder="Password"
+                    type="password"
+                    icon={FiLock}
+                    onChange={e => setPassword(e.target.value)}
+                />
+                <Button label="Sign up" onClick={handleSignUp}/>
                 <Link to="/">Log in</Link>
             </Form>
             
