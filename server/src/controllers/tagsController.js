ed6337c791f915e447dd7f2ab9e642a6e00fd12d
Daniela <trindadedv@gmail.com>
2023-06-23 18:11:47 +0200

    keeps list from showing duplicated tags

--- server/src/controllers/tagsController.js
+++ server/src/controllers/tagsController.js
@@ -3,7 +3,9 @@ const knex = require("../database/knex");
 class TagsController {
     async index(request, response) {
         const user_id = request.user.id;
-        const tags = await knex("tags").where({user_id})
+        const tags = await knex("tags")
+        .where({user_id})
+        .groupBy("name") //avoid listing duplicated tags in the home menu
 
         return response.json(tags)
     };
