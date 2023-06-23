861166d9b8890e87f15caf74e5e1b5e1004d9769
Daniela <trindadedv@gmail.com>
2023-06-23 20:42:48 +0200

    fixes logout from note details page

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,11 +1,17 @@
-import { RiShutDownLine } from "react-icons/ri";
-import { useAuth } from "../../hooks/auth";
-import { api } from "../../../../server/src/services/api";
 import avatarPlaceholder from "../../assets/profile-placeholder.jpg";
+import { api } from "../../../../server/src/services/api";
 import { Container, Profile, Logout } from "./styles";
+import { RiShutDownLine } from "react-icons/ri";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../hooks/auth";
 
 export function Header(){
     const {logOut, user} = useAuth();
+    const navigate = useNavigate();
+    function handleLogOut(){
+        navigate("/")
+        logOut();
+    }
     const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
     return(
         <Container>
@@ -16,7 +22,7 @@ export function Header(){
                     <strong>{user.name}</strong>
                 </div>
             </Profile>
-            <Logout onClick={logOut}>
+            <Logout onClick={handleLogOut}>
                 <RiShutDownLine/>
             </Logout>
         </Container>
